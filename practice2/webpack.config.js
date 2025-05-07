727d2d78ba67823d663fb3cdacd2a33fe1c772da
elsonkong0212 <elsonkong0212@gmail.com>
2025-05-08 00:30:06 +0800

    try to deploy on github

--- practice2/webpack.config.js
+++ practice2/webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath:'/HTML_learning/practice2/',
     clean: true,
   },
   mode: 'development',
