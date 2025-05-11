aa459fc6aa5eb42b91ba2e886c4a273702147841
elsonkong0212 <elsonkong0212@gmai.com>
2025-05-11 15:16:28 +0800

    base root fix2

--- practice3/my-todo-app/vite.config.js
+++ practice3/my-todo-app/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/HTML_learning/practice3/my-todo-app/',
+  base: '/HTML_learning/',
   plugins: [react()],
 })
